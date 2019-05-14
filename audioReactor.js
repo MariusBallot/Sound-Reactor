e855997a0e273fc3de8a5b7915e89accc4714507
marius ballot <ballot.marius@gmail.com>
2019-05-14 17:01:02 +0200

    first commit

---
+++ audioReactor.js
@@ -0,0 +1,39 @@
+class AudioReactor {
+
+    constructor() {
+        this.ctx
+        this.audio
+        this.audioSource
+        this.analyser
+        this.fdata
+
+        this.bind()
+    }
+
+    init() {
+        this.ctx = new AudioContext();
+        this.audio = document.querySelector('#audio');
+        this.audioSource = this.ctx.createMediaElementSource(this.audio);
+        this.analyser = this.ctx.createAnalyser();
+        this.analyser.smoothingTimeConstant = 0.8
+
+        this.audioSource.connect(this.analyser);
+        this.audioSource.connect(this.ctx.destination);
+        this.fdata = new Uint8Array(this.analyser.frequencyBinCount);
+
+        this.audio.play()
+    }
+
+    update() {
+        this.analyser.getByteFrequencyData(this.fdata);
+
+    }
+
+    bind() {
+        this.update = this.update.bind(this)
+        this.init = this.init.bind(this)
+    }
+
+}
+
+export { AudioReactor as default }
